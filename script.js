9747987ed360c9a1cdda0057e46c928f7c29aafd
IBRAHIM AJALA <79182423+Imoleayo64@users.noreply.github.com>
2025-08-03 23:16:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 async function getWeather() {
-  const city = document.getElementById("cityInput").value;
-  const apiKey = "https://api.open-meteo.com/v1/forecast?latitude=7.38&longitude=3.93&current=temperature_2m";
-  
-  const response = await fetch(apiKey);
-  const data = await response.json();
+  const url = 'https://api.open-meteo.com/v1/forecast?latitude=7.38&longitude=3.93&current=temperature_2m';
 
-  document.getElementById("weatherResult").innerHTML =
-    `<p>Temperature in Ibadan: ${data.current.temperature_2m}°C</p>`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const temp = data.current.temperature_2m;
+
+    document.getElementById('weatherResult').innerHTML =
+      `<p>🌤 Temperature in Ibadan: <strong>${temp}°C</strong></p>`;
+  } catch (error) {
+    document.getElementById('weatherResult').innerHTML =
+      `<p>Something went wrong. Please try again later.</p>`;
+  }
 }
